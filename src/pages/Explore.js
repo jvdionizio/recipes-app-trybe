f4de17c53cc183c0d1e244183c607f783ad2721a
Tomas Breuckmann <tomas.yoga@yahoo.com>
2022-04-29 15:20:21 -0300

    explore pages e explore food and drinks pages - criterios 67 até 74

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
 
-function Explore() {
+function Explore({ history }) {
   return (
     <div>
       <Header headerTitle="Explore" noSearch />
       <button
         type="button"
         data-testid="explore-foods"
+        onClick={ () => history.push('/explore/foods') }
       >
         Explore Foods
       </button>
       <button
         type="button"
         data-testid="explore-drinks"
+        onClick={ () => history.push('/explore/drinks') }
       >
         Explore Drinks
       </button>
@@ -22,5 +25,9 @@ function Explore() {
     </div>
   );
 }
+
+Explore.propTypes = {
+  history: PropTypes.func.isRequired,
+};
 
 export default Explore;
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
+import { getRandomMeal } from '../helpers/TheMealDBAPI';
 
-function ExploreFoods() {
+function ExploreFoods({ history }) {
+  const getSurpriseFood = async () => {
+    const response = await getRandomMeal();
+    const id = response[0].idMeal;
+    history.push(`/foods/${id}`);
+  };
+
   return (
     <div>
       <Header headerTitle="Explore Foods" noSearch />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => getSurpriseFood() }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
 }
+
+ExploreFoods.propTypes = {
+  history: PropTypes.func.isRequired,
+};
 
 export default ExploreFoods;
